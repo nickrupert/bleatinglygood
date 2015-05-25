d20a416a79149a7f8cccb21e40e3b7fefdb10ebb
Nick <rupertn@umich.edu>
2015-05-25 14:06:36 -0400

    second page functionality

--- index.php
+++ index.php
@@ -10,5 +10,7 @@
     echo "Hello, friends!";
 ?>
 
+<a href="http://snacksfriends.herokuapp.com/page2.php">This should go to page 2</a>
+
 </body>
 </html>
---
+++ page2.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+    <?php
+        echo "Did we make it to page 2?"
+    ?>
+
+
+</body>
+
+
+</html>
